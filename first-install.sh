ecbb403b3e77e7e5ca457c1d63edd3767823d439
pchaos <drifthua@gmail.com>
2022-10-13 17:17:16 +0800

    update:

--- first-install.sh
+++ first-install.sh
@@ -1,4 +1,7 @@
-# 
+#!/bin/bash
+#:che 
+dotfilepath="~/.dotfiles"
+
 rm ~/.bashrc
 rm ~/.bash_profile
 rm ~/.ssh/known_hosts
@@ -10,18 +13,24 @@ mkdir -p ~/openwrt/
 
 # download obsidian
 cd ~/.local/bin/ && wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.15.9/Obsidian-0.15.9.AppImage && ln -s Obsidian-0.15.9.AppImage obsidian
+# download alacritty
+wget -O ~/.dotfiles/bash/.alacritty.bash https://github.com/alacritty/alacritty/raw/master/extra/completions/alacritty.bash
 
 conda create -n hikyuu python==3.9
 conda create -n pytest python==3.9
 
-dnf install -y ruby cpan julia black ruby-devel node yarnpkg
-# dnf install -y xset 
+dnf install -y fish luarocks ruby cpan julia black ruby-devel nodejs yarnpkg alacritty coreutils
+npm install -g tree-sitter
 npm install -g neovim
 npm install -g @fsouza/prettierd
 npm install -g prettier
 LD_LIBRARY_PATH=/usr/local/lib64:/usr/lib64:/usr/lib64/mysql
 gem install neovim
 pip install isort
+# oh-my-fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+# fisher
+proxychains curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 # source code pro fonts
 FONT_NAME="SourceCodePro"
@@ -31,8 +40,8 @@ mkdir /tmp/adodefont
 cd /tmp/adodefont
 wget ${URL} -O ${FONT_NAME}.zip
 unzip -o -j ${FONT_NAME}.zip
-mkdir -p ~/.fonts
-cp *.otf ~/.fonts
+mkdir -p ~/.local/share/fonts
+cp *.otf ~/.local/share/fonts
 fc-cache -f -v
 
 # 显示器设置黑屏   设置-》显示器 -》内置显示器 -》不勾选
