889f06388be9a6a3959030805fc20d2a2bf771f2
pchaos <drifthua@gmail.com>
2025-07-20 22:00:35 +0800

    update

--- lvim/.config/lvim/init.lua
+++ lvim/.config/lvim/init.lua
@@ -1,5 +1,7 @@
--- last  Modified:   2025-07-17 19:22:01
+-- last  Modified:   2025-07-20 21:52:12
 -- vim.g.loaded_perl_provider = 0
 -- require("user.plugins.friendly-snippets").print_current_directory()
 vim.g.luasnip_log_level = "trace" -- or "debug"
-require("user.plugins.friendly-snippets").setup()
+-- require("user.plugins.friendly-snippets").setup()
+
+-- require('user.plugins.luasnip_config')
