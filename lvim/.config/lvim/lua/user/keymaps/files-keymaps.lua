3ceeb5907f0be9d554a5b06ff7bebbf0a810b9b1
pchaos <drifthua@gmail.com>
2025-07-29 23:02:26 +0800

    add copy file name key

--- lvim/.config/lvim/lua/user/keymaps/files-keymaps.lua
+++ lvim/.config/lvim/lua/user/keymaps/files-keymaps.lua
@@ -1,4 +1,6 @@
--- Modified: 2025-07-11 20:11:46
+-- Modified: 2025-07-29 18:08:58
+---- Description: This module sets up key mappings for copying file paths and names
+-- using the <leader>b prefix in normal mode. It integrates with
 local M = {}
 
 function M.setup()
@@ -8,6 +10,7 @@ function M.setup()
     a = { "<cmd>let @+ = expand('%:p')<cr>", "Copy full File Path" },
     -- 快捷键复制当前文件的路径（不包含文件名）
     p = { "<cmd>let @+ = expand('%:p:h')<cr>", "Copy File Path (Without File Name)" },
+    y = { "<cmd>let @+ = expand('%:t')<cr>", "Copy File Name" },
   }
 
   local status_ok, which_key = pcall(require, "which-key")
